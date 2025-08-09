d4fe60bdeecb994b0a9a76368eafedb3e27093df
AdemirAssisHonorato <156374058+AdemirAssisHonorato@users.noreply.github.com>
2025-08-09 19:54:25 +0000

    DESAFIO SUPER TRUNFO CONPARACAO

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,135 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+/* ATIVIDADE SUPER TRUNFO INTERMEDIARIO NIVEM BASICO
 
-int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+NOME: ADEMIR DE ASSIS DA SILVA GONÇALVES JUNIOR CURSO: ENGENHARIA DE SOFTWARE
 
+OBSERVAÇÕES: CONSEGUI ARRUMAR O SCANF PARA LER MAIS DE UM NOME.
+ESPECIFICADORES COLOCADOS COMO SOLICITADO
+*/
+
+int main(){
+//AQUI FICA AS VARIAVEIS
+
+int ponto_turistico, ponto_turistico_;    
+char estado[10], codigo[4], cidade[80], estado_[10], codigo_[4], cidade_[80]; 
+float area, pib, area_, pib_, den_pop1, den_pop2, pib_per1, pib_per2,super_poder, super_poder_;
+unsigned long int populacao, populacao_;
+
+//AQUI INICIA OS DADOS DA CARTA 1
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+printf("Insira aqui os dados da Carta1\n");
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+printf("Insira a abreviação do Estado para a sua carta\n");
+scanf("%s", estado);
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+printf("Insira um codigo entre  A01 e A04\n");
+scanf("%s", codigo);
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+printf("Insira o nome da cidade\n");
+scanf(" %[^\n]", cidade);
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+printf("Digite o numero da população\n");
+scanf("%ul", &populacao);
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+printf("Digite a área:\n");
+scanf("%f", &area);
 
-    return 0;
+printf("PIB:\n");
+scanf("%f", &pib);
+
+printf("Digite quantidade de pontos turisticos, no Estado\n");
+scanf("%d", &ponto_turistico);
+
+//AQUI INICIA OS DADOS CARTA 2
+
+printf("\n"); //ESTA É UMA QUEBRA DE LINHA
+
+printf("Insira aqui os dados da Carta2\n");
+
+printf("Insira a abreviação do Estado para a sua carta\n");
+scanf(" %s",estado_);
+
+printf("Insira um codigo entre  B01 e B04\n");
+scanf("%s", codigo_);
+
+printf("Insira o nome da cidade\n");
+scanf(" %[^\n]", cidade_);
+
+printf("Digite o numero da população\n");
+scanf("%ul", &populacao_);
+
+printf("Digite a área:\n");
+scanf("%f", &area_);
+
+printf("PIB:\n");
+scanf("%f", &pib_);
+
+printf("Digite quantidade de pontos turisticos, no Estado\n");
+scanf("%d", &ponto_turistico_);
+
+
+//AQUI INICIA O RESULTADO, SERÁ RESULTADO CARTA 1
+den_pop1 = (float)(populacao / area);
+den_pop2 = (float)(populacao_ / area_);
+
+pib_per1 = (float)(pib / populacao);
+pib_per2 = (float)(pib_ / populacao_);
+
+super_poder = (float)(populacao + area + pib + ponto_turistico + pib_per1) * (1.0 / den_pop1);// AQUI ESTA O CALCULO DO SUPER PODER 1
+super_poder_ = (float)(populacao_ + area_ + pib_ + ponto_turistico_ + pib_per2) * (1.0 / den_pop2);// AQUI ESTA O CALCULO DO SUPER PODER 2
+
+printf("\n"); //ESTA É UMA QUEBRA DE LINHA
+printf("Carta1\n");
+printf("\n"); //ESTA É UMA QUEBRA DE LINHA
+printf("Estado:%s\n", estado);
+printf("Codigo: %s\n", codigo);
+printf("Nome da cidade: %s\n", cidade);
+printf("População: %u milhões\n", populacao);
+printf("Área: %.2f km²\n", area);
+printf("PIB:%.2f R$\n", pib);
+printf("Pontos Turisticos:%d\n", ponto_turistico);
+printf("Densidade populacional 1: %.2f hab/km²\n", den_pop1);
+printf("PIB per capta1: %f R$\n", pib_per1);
+printf("Super Poder: %f\n", super_poder);
+
+//AQUI INICIA O RESULTADO CARTA 2
+
+printf("\n"); //ESTA É UMA QUEBRA DE LINHA
+printf("Carta2\n");
+printf("\n"); //ESTA É UMA QUEBRA DE LINHA
+printf("Estado:%s\n", estado_);
+printf("Codigo: %s\n", codigo_);
+printf("Nome da cidade: %s\n", cidade_);
+printf("População: %u milhões\n", populacao_);
+printf("Área: %.2f km²\n", area_);
+printf("PIB: %.2f R$\n", pib_);
+printf("Pontos Turisticos: %d\n", ponto_turistico_);
+printf("Densidade populacional 2: %.2f hab/km²\n", den_pop2); 
+printf("PIB per capta2: %f R$\n", pib_per2); 
+printf("Super Poder: %f\n", super_poder);
+
+// AQUI ESTA A COMPARAÇÃO BOOLEANA SIMPLES
+printf("\n"); //ESTA É UMA QUEBRA DE LINHA
+printf("RESULTADO COMPARAÇÃO!\n");
+printf("Populacao A > Populacao B: %d\n", populacao > populacao_);
+printf("Area A > Area B: %d\n", area > area_);
+printf("Pib A > Pib B: %d\n", pib > pib_);
+printf("Pontos turisticos A > Pontos turisticos B: %d\n", ponto_turistico > ponto_turistico_);
+printf("Densidade populaciol A < Densidade populacional B: %d\n", den_pop1 < den_pop2);
+printf("PIB per capta A > PIB per capta B: %d\n", pib_per1 > pib_per2);
+printf("Super Poder A > Super Poder B: %d\n", super_poder > super_poder_);
+printf("\n"); //ESTA É UMA QUEBRA DE LINHA
+printf("Atributo escolhido para Vitoria = SUPER PODER\n");
+
+//FUNÇÃO DE COMPARAÇÃO SIMPLES, CRITÉRIO DE DESEMPATE ESCOLHIDO SUPER PODER
+if(super_poder > super_poder_){
+    printf("CARTA 1 VENCEU!!!");
+}if(super_poder == super_poder_){
+    printf("EMPATE!!!");
+}else{
+    printf("CARTA 2 VENCEU!!!");
+}
+
+return 0;
 }
